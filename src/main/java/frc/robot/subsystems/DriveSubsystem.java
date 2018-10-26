240933a148708015a810e74d5bcb1260aa9d03d7
unknown <vk.soccerskillz@gmail.com>
2018-10-26 19:28:48 -0400

    This is code with adding 4 motor controls. ex.backleft,backright,frontleft,frontright

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -10,6 +10,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.commands.DriveTeleopCommand;
@@ -22,16 +23,25 @@ public class DriveSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  private WPI_TalonSRX m_left;
-  private WPI_TalonSRX m_right;
+  private SpeedControllerGroup m_left;
+  private WPI_TalonSRX m_frontleft;
+  private WPI_TalonSRX m_backleft;
+  private WPI_TalonSRX m_frontright;
+  private WPI_TalonSRX m_backright;
+  private SpeedControllerGroup m_right;
   private DifferentialDrive m_drive;
   //private OI m_oi = new OI();
   private Joystick m_js = new Joystick(0);
 
   public DriveSubsystem() {
-    m_left = new WPI_TalonSRX(1);
-    m_right = new WPI_TalonSRX(2);
-    // m_left.setInverted(true);  //invert left motor
+    m_frontleft = new WPI_TalonSRX(11);
+    m_backleft = new WPI_TalonSRX(13);
+    m_left = new SpeedControllerGroup(m_frontleft, m_backleft);
+
+    m_frontright = new WPI_TalonSRX(10);
+    m_backright = new WPI_TalonSRX(7);
+    m_right = new SpeedControllerGroup(m_frontright, m_backright);
+    // m_left.setInverted(true);  invert left motor
 
     m_drive = new DifferentialDrive(m_left, m_right);
     m_drive.arcadeDrive(0.0, 0.0);
